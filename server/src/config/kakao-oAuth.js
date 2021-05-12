02bfd9d6562a0cc2a81c7eef3fcb356a1cb1a0e8
Woogie <woogie@protopie.io>
2021-05-12 18:06:13 +0900

    환경변수 이름 바꿈:

--- server/src/config/kakao-oAuth.js
+++ server/src/config/kakao-oAuth.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const kakaoOAuthConfig = {
   clientId: process.env.KAKAO_CLIENT_ID,
-  redirectURL: process.env.NODE_ENV === "DEVELOPMENT" ? process.env.KAKAO_REDIRECT_DEV : process.env.KAKAO_REDIRECT_PRODUCTION,
+  redirectURL: process.env.KAKAO_REDIRECT,
 };
 
 module.exports = kakaoOAuthConfig;
--- server/src/util/envVariable.js
+++ server/src/util/envVariable.js
@@ -1,40 +1,16 @@
 const getEnvObj = () => {
   require("dotenv").config();
 
-  const {
-    NODE_ENV,
+  const { SERVER_PORT, DATABASE_NAME, DATABASE_ID, DATABASE_PASSWORD, HOST, DIALECT } = process.env;
+
+  return {
     SERVER_PORT,
     DATABASE_NAME,
-    DEV_DATABASE_ID,
-    DEV_DATABASE_PASSWORD,
-    DEV_HOST,
-    PRODUCTION_DATABASE_ID,
-    PRODUCTION_DATABASE_PASSWORD,
-    PRODUCTION_HOST,
+    DATABASE_ID: DATABASE_ID,
+    DATABASE_PASSWORD: DATABASE_PASSWORD,
+    HOST: HOST,
     DIALECT,
-  } = process.env;
-
-  if (NODE_ENV === "DEVELOPMENT") {
-    return {
-      SERVER_PORT,
-      DATABASE_NAME,
-      DATABASE_ID: DEV_DATABASE_ID,
-      DATABASE_PASSWORD: DEV_DATABASE_PASSWORD,
-      HOST: DEV_HOST,
-      DIALECT,
-    };
-  }
-
-  if (NODE_ENV === "PRODUCTION") {
-    return {
-      SERVER_PORT,
-      DATABASE_NAME,
-      DATABASE_ID: PRODUCTION_DATABASE_ID,
-      DATABASE_PASSWORD: PRODUCTION_DATABASE_PASSWORD,
-      HOST: PRODUCTION_HOST,
-      DIALECT,
-    };
-  }
+  };
 };
 
 module.exports = getEnvObj();
