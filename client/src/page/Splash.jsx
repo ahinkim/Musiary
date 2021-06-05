aae4d7cc5bdcec2b929138790250cf294d3b89a5
Woogie <woogie@protopie.io>
2021-06-05 20:20:23 +0900

    스플래쉬 수정

--- client/src/page/Splash.jsx
+++ client/src/page/Splash.jsx
@@ -1,29 +1,30 @@
 import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 import { color } from "../asset/palette";
 import Text from "../component/Text";
 import { content } from "../asset/Content";
 
-const GlobalStyle = createGlobalStyle`
-  body{
-    background:${color.BLUE};
-    background:linear-gradient(to bottom,${color.PINK} 42%,${color.BLUE});
-
-  }
-`;
 const CenterText = styled(Text)`
   margin-bottom: 10px;
   color: white;
   font-weight: 100;
   position: absolute;
-  top: 45vh;
-  left: 23vw;
+  top: 48vh;
+  left: 35vw;
+`;
+
+const SplashWrapper = styled.div`
+  position: absolute;
+  top: -5vw;
+  left: -5vh;
+  width: 110vw;
+  height: 110vh;
+  background: linear-gradient(to bottom, ${color.PINK} 42%, ${color.BLUE});
 `;
 export default function Splash() {
   return (
-    <>
-      <GlobalStyle />
+    <SplashWrapper>
       <CenterText size={50}>{content.MUSIARY_LOGO}</CenterText>
-    </>
+    </SplashWrapper>
   );
 }
